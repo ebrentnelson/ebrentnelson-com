fafa0b8f19f54b86956e174fb4a220749af60d9b
E Brent Nelson <ebrentnelson@gmail.com>
2025-08-13 14:27:37 -0600

    Start updating

--- js/default.js
+++
@@ -1,6 +0,0 @@
-(function(ebn, $, undefined) {
-    // initialize stuff
-    $(function(){
-        $('.year').text((new Date()).getFullYear()); 
-    });
-}(window.ebn = window.ebn || {}, jQuery));
--- js/home.js
+++ js/home.js
@@ -1,36 +1,50 @@
-(function(ebn, $, undefined) {
-    
-    var _timer;
-    
-    $.jQueryRandom = 0;
-    $.extend($.expr[":"], {
-        random: function(a, i, m, r) {
-            if (i == 0) {
-                $.jQueryRandom = Math.floor(Math.random() * r.length);
-            };
-            return i == $.jQueryRandom;
-        }
+(function(ebn, undefined) {
+
+  let _timer;
+
+  const getRandomElement = function(elements) {
+    if (elements.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * elements.length);
+    return elements[randomIndex];
+  };
+
+  const startBoogying = function() {
+    _timer = setInterval(function() {
+      const listItems = document.querySelectorAll('main ul li');
+
+      // Remove shaking class from all items
+      listItems.forEach(function(item) {
+        item.classList.remove('shaking');
+      });
+
+      // Add shaking class to random item
+      const randomItem = getRandomElement(listItems);
+      if (randomItem) {
+        randomItem.classList.add('shaking');
+      }
+    }, 2000);
+  };
+
+  ebn.init = function() {
+    const listItems = document.querySelectorAll('main ul li');
+
+    listItems.forEach(function(item) {
+      item.addEventListener('mouseover', function() {
+        listItems.forEach(function(li) {
+          li.classList.remove('shaking');
+        });
+        clearInterval(_timer);
+      });
+
+      item.addEventListener('mouseout', startBoogying);
     });
-    
-    var startBoogying = function() {
-        _timer = setInterval(function() {
-            $(".content ul li").removeClass("shaking");
-            $(".content ul li:random").addClass("shaking");
-        }, 2000);
-    };
-        
-    ebn.init = function() {
-        
-        $(".content ul li").mouseover(function(){ 
-            $(".content ul li").removeClass("shaking"); 
-            clearInterval(_timer); 
-        }).mouseout(startBoogying);
-            
-        startBoogying();
-    };
-    
-    // initialize stuff
-    $(function(){
-        ebn.init(); 
-    });
-}(window.ebn = window.ebn || {}, jQuery));
+
+    startBoogying();
+  };
+
+  // Initialize when DOM is ready
+  document.addEventListener('DOMContentLoaded', function() {
+    ebn.init();
+  });
+
+}(window.ebn = window.ebn || {}));
